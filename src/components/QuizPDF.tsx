f1a1a69ac5b76ffef1f024f8d79af36a3b50b8ae
djpelu <116528023+djpelu@users.noreply.github.com>
2025-09-15 13:42:03 +0200

    step x debug

--- src/components/QuizPDF.tsx
+++ src/components/QuizPDF.tsx
@@ -268,13 +268,13 @@ const QuizPDFDocument: React.FC<QuizPDFProps> = ({ quizData, scoresPDF, calcResu
                 {scoresPDF.map(({question, response = null, score}) => {
                   return (
                     <View key={question} style={styles.tableRow}>
-                      <View style={[styles.tableCell, { flex: 1, justifyContent: 'center', color: 'red'}]}>
+                      <View style={[styles.tableCell, { flex: 1, justifyContent: 'center', backgroundColor: 'red'}]}>
                         <Text style={styles.tableCellLabel}>{question || 'N/A'}</Text>
                       </View>
-                      <View style={[styles.tableCell, { flex: 1, justifyContent: 'center', alignItems: 'center', color:'blue' }]}>
+                      <View style={[styles.tableCell, { flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor:'blue' }]}>
                         <Text style={styles.tableCellValue}>{score || 'N/A'}</Text>
                       </View>
-                      <View style={[styles.tableCell, { flex: 2, justifyContent: 'center', color:'green' }]}>
+                      <View style={[styles.tableCell, { flex: 2, justifyContent: 'center', backgroundColor:'green' }]}>
                         <Text style={styles.tableCellValue}>{response || ''}</Text>
                       </View>
                     </View>
