62c0139fcde628e0b39fcd9232e8d8429a48d4af
djpelu <116528023+djpelu@users.noreply.github.com>
2025-09-16 13:05:44 +0200

    stampo selectedAnswers per fase test

--- src/pages/Quiz.tsx
+++ src/pages/Quiz.tsx
@@ -192,6 +192,9 @@ const Quiz = () => {
     // Salvo i risultati per il PDF
     setCalcResults(calcResults_data);
     setPrepScoresPDF(prepScoresPDF_data)
+
+    // stampo selectedAnswer a console che mi serve per test a foglio calc
+    console.log(selectedAnswers)
   };
 
   const resetQuiz = () => {
