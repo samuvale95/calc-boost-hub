a233c72111ff990f81ee02c75008720fd074125b
djpelu <116528023+djpelu@users.noreply.github.com>
2025-09-17 13:45:53 +0200

    CORRETTO CALCOLI ma ts potrebbe essere sbagliato

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -2,76 +2,82 @@ import pkg from "jstat"; // per distribuzione gamma
 const { jStat } = pkg;
 
 import table from "../data/calc_table.json" with { type: "json" } // parametri di pred
+import test_data from "../utils/test_data.json" with { type: "json" } // parametri di pred
 
-// creo funzione per calcolo del logaritmo naturale della media
-function calcLnMean(arr: number[]): number {
-  // calcolo media
+// creo funzione per calcolo meadia
+function calcMean(arr: number[]): number {
   const sum = arr.reduce((acc, val) => acc + val, 0);
-  const mean = sum / arr.length;
-
-  // calcolo ln(media)
+  const mean = (sum / arr.length);
+  
   if (mean === 0) {
-    return -4.8 // minimo ln(mean) possibile
+    return 0.0001 // chiedi conferma a Toraldo
   } else if (mean === 1) {
-    return 4.8 // massimo ln(mean) possibile
+    return 0.9999 // chiedi conferma a Toraldo
   } else {
-    return Math.log(mean)
+    return mean
   }
+}
+
+// creo funzione per calcolo logit
+function calcLogit(mean: number): number {
+  return Math.log(mean / (1- mean));
 }
 
 export function calcResults(answers: { [key: string]: any }): { [key: string]: number } {
   const responseArray: any[] = Object.values(answers);
-  const allLnMeans: { [key: string]: number } = {};
+  const allLogit: { [key: string]: number } = {};
 
   // calcolo ln(età in mesi) centrato
   const ageQ = "ETÀ in anni e mesi, es. se il paziente ha 3 anni e 4 mesi scrivere 3 nella casella 'Anni' e 4 in quella 'Mesi'"
   const ageItem = responseArray.find((item: any) => item.question === ageQ);
-  const age = ageItem?.Anni || 0 * 12 + ageItem?.Mesi || 0;
+  const age = ageItem.Anni * 12 + ageItem.Mesi;
   const meanLnAge = 4.944218314;
   const lnAge0 = Math.log(age) - meanLnAge;
 
   // estrapolo genere e nazionalità
   const sexItem = responseArray.find((item: any) => item.question === "GENERE");
   const natItem = responseArray.find((item: any) => item.question === "NAZIONALITÀ");
-  const sex = sexItem?.score || 0;
-  const nat = natItem?.score || 0;
+  const sex = sexItem.score;
+  const nat = natItem.score;
 
-  // calcolo ln(media) overall
+  // calcolo logit overall
   const overallItems = responseArray.filter((resp: any) => resp.subdom >= 1 && resp.subdom <= 17); // elimino subdom 0, 18, 19 
-  allLnMeans["Overall"] = calcLnMean(overallItems.map((item: any) => item.score)) // calcolo ln(media) e aggiungo a allLnMeans
+  const overallMean = calcMean(overallItems.map((item: any) => item.score)) // calcolo media overall
+  allLogit["Overall"] = calcLogit(overallMean) // calcolo logit e aggiungo a allLogit
 
-  // calcolo ln(medie) doms
-  const domNames = ["Mot", "Aut", "Lan", "Mem", "Emo"];
+  // calcolo logit doms
+  const domNames = ["Mot", "Aut", "Lan", "Mem", "Emo"]; // escludo Pers e Sleep
 
   for (const domName of domNames) {
     const items = responseArray.filter((resp: any) => resp.dom === domName); // filtro per dom
-    allLnMeans[domName] = calcLnMean(items.map((item: any) => item.score)); // calcolo ln(media) e aggiungo a allLnMeans
+    const domMean = calcMean(items.map((item: any) => item.score)) // calcolo media per dom
+    allLogit[domName] = calcLogit(domMean); // calcolo logit e aggiungo a allLogit
   }
 
-  // calcolo ln(medie) subdoms ESCLUSI 18 e 19
+  // calcolo logit subdoms ESCLUSI 18 e 19
   for (let i = 1; i <= 17; i++) {
-    const items = responseArray.filter((resp: any) => resp.subdom === i); // filtro per subdom
-    const mean = calcLnMean(items.map((item: any) => item.score)); // calcolo ln(media)
-    allLnMeans[`sub${i}`] = mean; // aggiungo ln(media) a allLnMeans
+    const items = responseArray.filter((resp: any) => resp.subdom === i); // filtro per subdom eslcudendo 0, 18, 19
+    const subMean = calcMean(items.map((item: any) => item.score)) // calcolo media per subdom
+    allLogit[`sub${i}`] = calcLogit(subMean);; // calcolo logit e aggiungo a allLogit
   }
 
-  // calcolo ln(media) subdom 18
-  allLnMeans["sub18"] = Math.log(responseArray.filter((resp: any) => resp.subdom === 18).map((resp: any) => resp.score).reduce((acc: number, val: number) => acc + val, 0) / 24);
+  // DA RIVEDERE SUBDOM 18
+  const meanSub18 = responseArray.filter((resp: any) => resp.subdom === 18).map((resp: any) => resp.score).reduce((acc: number, val: number) => acc + val, 0) / 24 // calcolo media subdom 18 con formula apposita
+  allLogit["sub18"] = calcLogit(meanSub18) // calcolo logit e aggiungo a allLogit
 
-  // calcolo percentile ESCLUSO subdom 19
+  // calcolo percentili ESCLUSO subdom 19
   const results: { [key: string]: number } = {}
 
-  for (const [key, value] of Object.entries(allLnMeans)) {
-    const dom = table.find((item: any) => item.dom === key); // trovo riga per overall/dom/subdom in table.js
-    const sd = dom?.sd || 1; // estrapolo sd
-    const pred = (dom?.int || 0) + ((dom?.s_age || 0) * lnAge0) + ((dom?.s_nat || 0) * nat) + ((dom?.s_sex || 0) * sex) + ((dom?.s_sexnat || 0) * sex * nat); // calcolo media prevista per la popolazione dei pari
+  for (const [key, value] of Object.entries(allLogit)) {
+    const dom = table.find((item: any) => item.dom === key); // trovo riga coefficienti per overall/dom/subdom in table.js
+    const sd = dom.sd; // estrapolo sd
+    const pred = (dom.int) + ((dom.s_age) * lnAge0) + ((dom.s_nat) * nat) + ((dom.s_sex) * sex) + ((dom.s_sexnat) * sex * nat); // calcolo media prevista per la popolazione dei pari
     const z = (value - pred) / sd; // calcolo z
     results[key] = jStat.normal.cdf(z, 0, 1) * 100; // calcolo percentile rispetto alla distribuzione normale standard
   }
 
   // calcolo percentile subdom 19
-  const subdom19Item = responseArray.find((item: any) => item.subdom === 19);
-  const nRisvegli = subdom19Item?.score || 0; // estrapolo score utente
+  const nRisvegli = responseArray.find((item: any) => item.subdom === 19).score;
   const sub19Mean = Math.exp(0.114 + (0.416 * lnAge0) + (0.559 * nat)); // calcolo media popolazione dei pari
   const alpha = 0.521648409;
   const beta = sub19Mean / alpha;
@@ -79,3 +85,5 @@ export function calcResults(answers: { [key: string]: any }): { [key: string]: n
 
   return results
 }
+
+console.log(calcResults(test_data))
