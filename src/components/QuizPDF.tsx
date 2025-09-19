b547d853e9e3536c05334a660bcdc65d36e70841
djpelu <116528023+djpelu@users.noreply.github.com>
2025-09-19 12:41:33 +0200

    colonne per z e percentile

--- src/components/QuizPDF.tsx
+++ src/components/QuizPDF.tsx
@@ -215,7 +215,10 @@ const QuizPDFDocument: React.FC<QuizPDFProps> = ({ quizData, scoresPDF, calcResu
                     <Text style={styles.tableCellLabel}>Overall</Text>
                   </View>
                   <View style={styles.tableCell}>
-                    <Text style={styles.tableCellValue}>z: {calcResults.Overall.find(el => 'z' in el)?.z || 'N/A'} percentile: {calcResults.Overall.find(el => 'p' in el)?.p || 'N/A'} </Text>
+                    <Text style={styles.tableCellValue}>z:  {calcResults.Overall.find(el => 'z' in el)?.z || 'N/A'} percentile: {calcResults.Overall.find(el => 'p' in el)?.p || 'N/A'} </Text>
+                  </View>
+                  <View style={styles.tableCell}>
+                    <Text style={styles.tableCellValue}>percentile:  {calcResults.Overall.find(el => 'p' in el)?.p || 'N/A'} </Text>
                   </View>
                 </View>
               </View>
@@ -237,7 +240,10 @@ const QuizPDFDocument: React.FC<QuizPDFProps> = ({ quizData, scoresPDF, calcResu
                       <Text style={styles.tableCellLabel}>{label}</Text>
                     </View>
                     <View style={styles.tableCell}>
-                      <Text style={styles.tableCellValue}>z: {calcResults[dom].find(el => 'z' in el)?.z || 'N/A'} percentile: {calcResults[dom].find(el => 'p' in el)?.p || 'N/A'}</Text>
+                      <Text style={styles.tableCellValue}>z:  {calcResults[dom].find(el => 'z' in el)?.z || 'N/A'}</Text>
+                    </View>
+                    <View style={styles.tableCell}>
+                      <Text style={styles.tableCellValue}>percentile:  {calcResults[dom].find(el => 'p' in el)?.p || 'N/A'}</Text>
                     </View>
                   </View>
                 ))}
@@ -274,7 +280,10 @@ const QuizPDFDocument: React.FC<QuizPDFProps> = ({ quizData, scoresPDF, calcResu
                       <Text style={styles.tableCellLabel}>{label}</Text>
                     </View>
                     <View style={styles.tableCell}>
-                      <Text style={styles.tableCellValue}>z: {calcResults[dom].find(el => 'z' in el)?.z || 'N/A'} percentile: {calcResults[dom].find(el => 'p' in el)?.p || 'N/A'}</Text>
+                      <Text style={styles.tableCellValue}>z:  {calcResults[dom].find(el => 'z' in el)?.z || 'N/A'}</Text>
+                    </View>
+                    <View style={styles.tableCell}>
+                      <Text style={styles.tableCellValue}>percentile:  {calcResults[dom].find(el => 'p' in el)?.p || 'N/A'}</Text>
                     </View>
                   </View>
                 ))}
