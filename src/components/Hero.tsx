991bdf3aeaa9d06009d69673e7dfa895b8b1f43f
djpelu <116528023+djpelu@users.noreply.github.com>
2025-09-30 16:28:39 +0200

    prima sezione home: rimetto titolo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,9 +8,9 @@ export const Hero = () => {
       <div className="relative z-10 container mx-auto px-4 text-center">
         <div className="max-w-4xl mx-auto">
           <img src="/logo.jpg" alt="logo D-DAND" className="mx-auto" />
-          {/* <h1 className="text-5xl md:text-7xl font-bold mb-4 bg-gradient-to-r from-foreground to-primary-glow bg-clip-text text-transparent">
-            D-DAND
-          </h1> */}
+          <h1 className="text-5xl md:text-7xl font-bold mb-4 bg-gradient-to-r from-foreground to-primary-glow bg-clip-text text-transparent">
+            Dravet Disease Associated Neuropsychiatric Disorder
+          </h1>
           <p className="text-xl md:text-2xl text-muted-foreground mb-6 max-w-2xl mx-auto">
             Per una valutazione completa della sindrome di Dravet
           </p>
