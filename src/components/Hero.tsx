03580512cfb480ba0f9426a4cf5a0f90a6a69936
djpelu <116528023+djpelu@users.noreply.github.com>
2025-10-24 16:34:57 +0200

    home: logo più grande

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export const Hero = () => {
     <section className="relative bg-[#ffffff] min-h-[80vh] flex items-center justify-center overflow-hidden">
       <div className="relative z-10 container mx-auto px-4 text-center">
         <div className="max-w-4xl mx-auto">
-          <img src="/logo.png" alt="logo D-DAND" className="w-48 md:w-64 mx-auto" />
+          <img src="/logo.png" alt="logo D-DAND" className="w-48 md:w-64 mb:4 mx-auto" />
           <h1 className="text-4xl md:text-6xl font-bold mb-4 bg-gradient-to-r from-foreground to-primary-glow bg-clip-text text-transparent">
             D-DAND
           </h1>
